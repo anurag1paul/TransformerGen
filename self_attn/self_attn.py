2b0a2a2b46450c554efcd7978edcbeb09a6c3226
Anurag Paul <anurag1paul@gmail.com>
2019-03-04 14:30:18 -0800

    implemented self attn gan

--- self_attn/self_attn.py
+++ self_attn/self_attn.py
@@ -1,41 +1,74 @@
-# https://github.com/heykeetae/Self-Attention-GAN/blob/master/sagan_models.py
+# https://github.com/voletiv/self-attention-GAN-pytorch/blob/master/sagan_models.py
 
+import numpy as np
 import torch
-from torch.nn import Module
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.nn.utils import spectral_norm
+from torch.nn.init import xavier_uniform_
 
 
-class SelfAttn(Module):
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.)
+
+
+def snconv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+    return spectral_norm(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                   stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias))
+
+
+def snlinear(in_features, out_features):
+    return spectral_norm(nn.Linear(in_features=in_features, out_features=out_features))
+
+
+def sn_embedding(num_embeddings, embedding_dim):
+    return spectral_norm(nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim))
+
+
+class Self_Attn(nn.Module):
     """ Self attention Layer"""
 
-    def __init__(self,in_dim,activation):
-        super(SelfAttn,self).__init__()
-        self.chanel_in = in_dim
-        self.activation = activation
+    def __init__(self, in_channels):
+        super(Self_Attn, self).__init__()
+        self.in_channels = in_channels
+        self.snconv1x1_theta = snconv2d(in_channels=in_channels, out_channels=in_channels//8, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_phi = snconv2d(in_channels=in_channels, out_channels=in_channels//8, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_g = snconv2d(in_channels=in_channels, out_channels=in_channels//2, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_attn = snconv2d(in_channels=in_channels//2, out_channels=in_channels, kernel_size=1, stride=1, padding=0)
+        self.maxpool = nn.MaxPool2d(2, stride=2, padding=0)
+        self.softmax  = nn.Softmax(dim=-1)
+        self.sigma = nn.Parameter(torch.zeros(1))
 
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-        self.softmax  = nn.Softmax(dim=-1) #
-
-    def forward(self,x):
+    def forward(self, x):
         """
             inputs :
-                x : input feature maps( B X C X W X H)
+                x : input feature maps(B X C X W X H)
             returns :
                 out : self attention value + input feature
                 attention: B X N X N (N is Width*Height)
         """
-        batchsize, C, width, height = x.size()
-        proj_query = self.query_conv(x).view(batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
-        proj_key = self.key_conv(x).view(batchsize,-1,width*height) # B X C x (*W*H)
-        energy = torch.bmm(proj_query,proj_key) # transpose check
-        attention = self.softmax(energy) # BX (N) X (N)
-        proj_value = self.value_conv(x).view(batchsize,-1,width*height) # B X C X N
-
-        out = torch.bmm(proj_value,attention.permute(0,2,1))
-        out = out.view(batchsize,C,width,height)
-
-        out = self.gamma * out + x
-        return out,attention
+        _, ch, h, w = x.size()
+        # Theta path
+        theta = self.snconv1x1_theta(x)
+        theta = theta.view(-1, ch//8, h*w)
+        # Phi path
+        phi = self.snconv1x1_phi(x)
+        phi = self.maxpool(phi)
+        phi = phi.view(-1, ch//8, h*w//4)
+        # Attn map
+        attn = torch.bmm(theta.permute(0, 2, 1), phi)
+        attn = self.softmax(attn)
+        # g path
+        g = self.snconv1x1_g(x)
+        g = self.maxpool(g)
+        g = g.view(-1, ch//2, h*w//4)
+        # Attn_g
+        attn_g = torch.bmm(g, attn.permute(0, 2, 1))
+        attn_g = attn_g.view(-1, ch//2, h, w)
+        attn_g = self.snconv1x1_attn(attn_g)
+        # Out
+        out = x + self.sigma*attn_g
+        return out
---
+++ train_self_attn_gan.py
@@ -0,0 +1,27 @@
+import torch
+
+from easydict import EasyDict
+
+from data_loader import CubDataset
+from data_preprocess import DataPreprocessor
+from self_attn.self_attn_gan import SelfAttnGAN
+from utils import get_opts
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+output_dir = "checkpoints/selfAttnGAN"
+data_dir = 'dataset/'
+epoch_file = "epoch.txt"
+log_file = "logs.log"
+
+opts = EasyDict(get_opts("config/bird.yaml"))
+
+preprocessor = DataPreprocessor("cub", data_dir)
+ixtoword = preprocessor.get_idx_to_word()
+train_set = CubDataset(preprocessor, opts, mode='train')
+val_set = CubDataset(preprocessor, opts, mode='val')
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=opts.TRAIN.BATCH_SIZE, shuffle=True, drop_last=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=opts.TRAIN.BATCH_SIZE, shuffle=True, drop_last=True)
+
+self_attn_gan = SelfAttnGAN(device, output_dir, opts, ixtoword, train_loader, val_loader)
+self_attn_gan.train()
